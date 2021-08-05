a8bd514edd669a6a191ec9547d58b8a48e52f6f3
maccam912 <maccam912@gmail.com>
2021-08-04 21:45:04 -0500

    closing positions

--- lib/tradiex/trading.ex
+++ lib/tradiex/trading.ex
@@ -9,7 +9,7 @@ defmodule Tradiex.Trading do
   Submit an equity order. Accepts options price, stop, and tag
 
     iex> %{"account_number" => acct} = Tradiex.Account.get_user_profile()
-    iex> %{"id" => _id, "status" => status, "partner_id" => _partner_id} = Tradiex.Trading.post_order(acct, "equity", "AAPL", "buy", 1, "limit", "day", price: 10.00, tag: "example-tag")
+    iex> %{"id" => _id, "status" => status, "partner_id" => _partner_id} = Tradiex.Trading.post_order(acct, "equity", "AAPL", "buy", 1, "limit", "day", price: 1.00, tag: "example-tag")
     iex> status
     "ok"
   """
@@ -34,5 +34,18 @@ defmodule Tradiex.Trading do
       Tradiex.request(:post, "accounts/#{account_id}/orders", URI.encode_query(query), %{})
 
     order
+  end
+
+  def equity_market_order(account_id, symbol, quantity) do
+    post_order(account_id, "equity", symbol, "buy", quantity, "market", "day")
+  end
+
+  def equity_position_close(account_id, symbol) do
+    [position] =
+      Tradiex.Account.get_positions(account_id)
+      |> Enum.filter(fn position -> Map.get(position, "symbol") == symbol end)
+
+    size = Map.get(position, "quantity")
+    equity_market_order(account_id, symbol, -1 * size)
   end
 end
